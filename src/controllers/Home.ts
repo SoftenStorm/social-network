727320381b1e18afb1baa4d0b2344ff2862208a7
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-09-01 19:13:30 +0700

    Composing - understand development process

--- src/controllers/Home.ts
+++ src/controllers/Home.ts
@@ -3,9 +3,13 @@
 
 import {Request, Response} from "express";
 import Componentindex from "./components/index.js";
+import Component26645199 from "./components/guide/development_process/_26645199.js";
 
 export const index = (req: Request, res: Response) => {
 	new Componentindex(req, res, "home/index");
+}
+export const _26645199 = (req: Request, res: Response) => {
+	new Component26645199(req, res, "home/guide/development_process/_26645199");
 }
 
 // <--- Auto[Generating:V1]
--- src/route.ts
+++ src/route.ts
@@ -6,6 +6,8 @@ import * as homeController from './controllers/Home.js';
 const route = (app: any) => {
  app.get("/", homeController.index);
  app.post("/", homeController.index);
+ app.get("/guide/development_process", homeController._26645199);
+ app.post("/guide/development_process", homeController._26645199);
 }
 
 export default route;
