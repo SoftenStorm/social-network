82e963861096902b6afe76ba6f39c741aef1d227
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-09-02 15:45:48 +0700

    Order pages using prefixes.

--- src/controllers/Home.ts
+++ src/controllers/Home.ts
@@ -3,21 +3,21 @@
 
 import {Request, Response} from "express";
 import Componentindex from "./components/index.js";
-import Component26645199 from "./components/guide/development_process/_26645199.js";
-import Component08d99631 from "./components/guide/testing_and_deployment/_08d99631.js";
-import Component6c6c35a0 from "./components/guide/html5_and_css3_tutorial/_6c6c35a0.js";
+import Component26645199 from "./components/guide/s01_development_process/_26645199.js";
+import Component08d99631 from "./components/guide/s02_testing_and_deployment/_08d99631.js";
+import Component6c6c35a0 from "./components/guide/s03_html5_and_css3_tutorial/_6c6c35a0.js";
 
 export const index = (req: Request, res: Response) => {
 	new Componentindex(req, res, "home/index");
 }
 export const _26645199 = (req: Request, res: Response) => {
-	new Component26645199(req, res, "home/guide/development_process/_26645199");
+	new Component26645199(req, res, "home/guide/s01_development_process/_26645199");
 }
 export const _08d99631 = (req: Request, res: Response) => {
-	new Component08d99631(req, res, "home/guide/testing_and_deployment/_08d99631");
+	new Component08d99631(req, res, "home/guide/s02_testing_and_deployment/_08d99631");
 }
 export const _6c6c35a0 = (req: Request, res: Response) => {
-	new Component6c6c35a0(req, res, "home/guide/html5_and_css3_tutorial/_6c6c35a0");
+	new Component6c6c35a0(req, res, "home/guide/s03_html5_and_css3_tutorial/_6c6c35a0");
 }
 
 // <--- Auto[Generating:V1]
--- src/route.ts
+++ src/route.ts
@@ -6,12 +6,12 @@ import * as homeController from './controllers/Home.js';
 const route = (app: any) => {
  app.get("/", homeController.index);
  app.post("/", homeController.index);
- app.get("/guide/development_process", homeController._26645199);
- app.post("/guide/development_process", homeController._26645199);
- app.get("/guide/testing_and_deployment", homeController._08d99631);
- app.post("/guide/testing_and_deployment", homeController._08d99631);
- app.get("/guide/html5_and_css3_tutorial", homeController._6c6c35a0);
- app.post("/guide/html5_and_css3_tutorial", homeController._6c6c35a0);
+ app.get("/guide/s01_development_process", homeController._26645199);
+ app.post("/guide/s01_development_process", homeController._26645199);
+ app.get("/guide/s02_testing_and_deployment", homeController._08d99631);
+ app.post("/guide/s02_testing_and_deployment", homeController._08d99631);
+ app.get("/guide/s03_html5_and_css3_tutorial", homeController._6c6c35a0);
+ app.post("/guide/s03_html5_and_css3_tutorial", homeController._6c6c35a0);
 }
 
 export default route;
