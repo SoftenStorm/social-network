8b44e7b91c6e046a8e8999c6dcc11e5da1a5b7a7
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-09-05 20:48:58 +0700

    Add the rest of pages.

--- src/controllers/Home.ts
+++ src/controllers/Home.ts
@@ -6,6 +6,11 @@ import Componentindex from "./components/index.js";
 import Component26645199 from "./components/guide/s01_development_process/_26645199.js";
 import Component08d99631 from "./components/guide/s02_testing_and_deployment/_08d99631.js";
 import Component6c6c35a0 from "./components/guide/s03_html5_and_css3_tutorial/_6c6c35a0.js";
+import Componentddb9b66a from "./components/guide/s04_responsive_layout_tutorial/_ddb9b66a.js";
+import Component99aa9e89 from "./components/guide/s05_reusable_component_tutorial/_99aa9e89.js";
+import Component80dd1ceb from "./components/guide/s06_data_modelling_tutorial/_80dd1ceb.js";
+import Component914d6b41 from "./components/guide/s07_about_dot_notation/_914d6b41.js";
+import Component28517bb5 from "./components/guide/s08_about_coding/_28517bb5.js";
 
 export const index = (req: Request, res: Response) => {
 	new Componentindex(req, res, "home/index");
@@ -18,6 +23,21 @@ export const _08d99631 = (req: Request, res: Response) => {
 }
 export const _6c6c35a0 = (req: Request, res: Response) => {
 	new Component6c6c35a0(req, res, "home/guide/s03_html5_and_css3_tutorial/_6c6c35a0");
+}
+export const _ddb9b66a = (req: Request, res: Response) => {
+	new Componentddb9b66a(req, res, "home/guide/s04_responsive_layout_tutorial/_ddb9b66a");
+}
+export const _99aa9e89 = (req: Request, res: Response) => {
+	new Component99aa9e89(req, res, "home/guide/s05_reusable_component_tutorial/_99aa9e89");
+}
+export const _80dd1ceb = (req: Request, res: Response) => {
+	new Component80dd1ceb(req, res, "home/guide/s06_data_modelling_tutorial/_80dd1ceb");
+}
+export const _914d6b41 = (req: Request, res: Response) => {
+	new Component914d6b41(req, res, "home/guide/s07_about_dot_notation/_914d6b41");
+}
+export const _28517bb5 = (req: Request, res: Response) => {
+	new Component28517bb5(req, res, "home/guide/s08_about_coding/_28517bb5");
 }
 
 // <--- Auto[Generating:V1]
--- src/route.ts
+++ src/route.ts
@@ -12,6 +12,16 @@ const route = (app: any) => {
  app.post("/guide/s02_testing_and_deployment", homeController._08d99631);
  app.get("/guide/s03_html5_and_css3_tutorial", homeController._6c6c35a0);
  app.post("/guide/s03_html5_and_css3_tutorial", homeController._6c6c35a0);
+ app.get("/guide/s04_responsive_layout_tutorial", homeController._ddb9b66a);
+ app.post("/guide/s04_responsive_layout_tutorial", homeController._ddb9b66a);
+ app.get("/guide/s05_reusable_component_tutorial", homeController._99aa9e89);
+ app.post("/guide/s05_reusable_component_tutorial", homeController._99aa9e89);
+ app.get("/guide/s06_data_modelling_tutorial", homeController._80dd1ceb);
+ app.post("/guide/s06_data_modelling_tutorial", homeController._80dd1ceb);
+ app.get("/guide/s07_about_dot_notation", homeController._914d6b41);
+ app.post("/guide/s07_about_dot_notation", homeController._914d6b41);
+ app.get("/guide/s08_about_coding", homeController._28517bb5);
+ app.post("/guide/s08_about_coding", homeController._28517bb5);
 }
 
 export default route;
