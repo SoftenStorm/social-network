98dfcabd3e84f303af95531fa4067e8bed7ef9cc
social-cm <120189102+social-cm@users.noreply.github.com>
2022-12-10 11:44:06 +0700

    [continue] Update fond button style.

--- src/public/js/components/Project.Controls.FlowLayout_45655143.tsx
+++ src/public/js/components/Project.Controls.FlowLayout_45655143.tsx
@@ -357,7 +357,7 @@ class FlowLayout_45655143 extends Base {
                             if this.hasFondOf(i)
                               .internal-fsb-element(style={'color': 'rgba(0, 94, 255, 1)', 'display': 'inline-block', 'marginLeft': '5px', 'position': 'relative', 'top': '-2px'}, dangerouslySetInnerHTML={__html: "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-box2-heart-fill\" viewBox=\"0 0 16 16\">\n<path d=\"M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4h-8.5ZM8.5 4h6l.5.667V5H1v-.333L1.5 4h6V1h1v3ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z\"/>\n</svg>"}, internal-fsb-guid="c4e84172")
                           .internal-fsb-element(style={'WebkitFlexBasis': '0px', 'WebkitFlexGrow': '1', 'flexBasis': '0px', 'flexGrow': '1', 'fontSize': '13px', 'lineHeight': '26px', 'paddingLeft': '8px', 'paddingRight': '0px'}, internal-fsb-guid="ac71a3c1")
-                            | จำนวนผู้ที่ชื่นชอบ #{this.getTotalAction(1, i)}
+                            | จำนวนผู้ที่ชื่นชอบ #{this.getTotalAction(1, i)} คน 
                       .-fsb-preset-bb841183.internal-fsb-element.internal-fsb-strict-layout(style={'FsbInheritedPresets': 'bb841183', 'paddingLeft': '15px', 'paddingRight': '15px'}, internal-fsb-class="FlowLayout", internal-fsb-guid="ed431ea8")
                         input.col-12.internal-fsb-element(style={'FsbCodeLock': '1'}, type="hidden", internal-fsb-guid="bec0942b")
                         input.col-12.internal-fsb-element(style={'FsbCodeLock': '1'}, type="hidden", value=this.getDataFromNotation("Post[" + i + "].id"), internal-fsb-guid="b5b68759")
